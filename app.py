b263c3d5a6f765e4cd503417ee6f263bd98f5ff5
cavein <cavein@localhost.localdomain>
2019-11-05 16:00:25 +0300

    Get and Create working

---
+++ app.py
@@ -0,0 +1,94 @@
+from flask import Flask,request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os 
+
+
+#init app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+#Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+#init db & marsh
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+#Product Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    desc = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self, name, desc, price, qty):
+        self.name = name
+        self.desc = desc
+        self.price = price
+        self.qty = qty
+
+#Product Schema
+class ProductSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'desc', 'price', 'qty')
+
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
+
+
+#Create a product
+@app.route('/product', methods=['POST'])
+def add_products():
+    name = request.json['name']
+    desc = request.json['desc']
+    price = request.json['price']
+    qty = request.json['qty']
+
+    new_product = Product(name,desc,price,qty)
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
+
+#Get all products
+@app.route('/product', methods=['GET'])
+def get_all_products():
+    all_products = Product.query.all()
+    result = products_schema.dump(all_products)
+    return jsonify(result)
+
+#Update Product
+@app.route('/product/<int:id>', methods=['PUT'])
+def update_product(id):
+    product = Product.query.get(id)
+
+    name = request.json['name']
+    desc = request.json['desc']
+    price = request.json['price']
+    qty = request.json['qty']
+
+    product.name = name 
+    product.desc = desc
+    product.price = price
+    product.qty = qty
+
+    db.session.commit()
+
+    return product_schema.jsonify(product)
+
+#Delete a product
+@app.route('/product/<int:id>', methods=['DELETE'])
+def delete_product(id):
+    product = Product.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+
+    return product_schema.jsonify(product)
+
+if __name__ == '__main__':
+    app.run(debug=True)
